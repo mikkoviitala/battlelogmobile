529edb54b418a4a105d0b0660b95cae1245fd917
Mikko Viitala <mikko.viitala@nbl.fi>
2012-06-15 09:23:45 +0300

    Removed credentials encryption because of problems some users have, tried to keep it "backward compatible" for the users having no issues, t

--- BattlelogMobile.Core/Repository/FileCredentialsRepository.cs
+++ BattlelogMobile.Core/Repository/FileCredentialsRepository.cs
@@ -9,7 +9,6 @@ namespace BattlelogMobile.Core.Repository
     public class FileCredentialsRepository : ICredentialsRepository 
     {
         private readonly CryptoService _crypto = new CryptoService();
-        private bool _isEncrypted;
 
         public FileCredentialsRepository() : this(IsolatedStorageFile.GetUserStoreForApplication())
         {}
@@ -24,14 +23,16 @@ namespace BattlelogMobile.Core.Repository
 
         public void Save(ICredentials credentials)
         {
-            using (var writer = new StreamWriter(StorageFile.OpenFile(Common.CredentialsFile, FileMode.OpenOrCreate, FileAccess.Write)))
+            using (var writer = new StreamWriter(StorageFile.OpenFile(Common.CredentialsFile, FileMode.Create, FileAccess.Write)))
             {
-                writer.WriteLine(_crypto.Encrypt(credentials.Email));
-                writer.WriteLine(_crypto.Encrypt(credentials.Password));
+                //writer.WriteLine(_crypto.Encrypt(credentials.Email));
+                //writer.WriteLine(_crypto.Encrypt(credentials.Password));
+                writer.WriteLine(credentials.Email);
+                writer.WriteLine(credentials.Password);
                 writer.WriteLine(credentials.RememberMe);
                 writer.Close();
             }
-            IsolatedStorageSettings.ApplicationSettings["IsEncrypted"] = true;
+            IsolatedStorageSettings.ApplicationSettings["IsEncrypted"] = false;
         }
 
         public ICredentials Load()
@@ -39,17 +40,18 @@ namespace BattlelogMobile.Core.Repository
             ICredentials credentials = new Credentials();
             using (var reader = new StreamReader(StorageFile.OpenFile(Common.CredentialsFile, FileMode.OpenOrCreate, FileAccess.Read)))
             {
-                IsolatedStorageSettings.ApplicationSettings.TryGetValue("IsEncrypted", out _isEncrypted);
+                bool isEncrypted;
+                IsolatedStorageSettings.ApplicationSettings.TryGetValue("IsEncrypted", out isEncrypted);
                 if (reader.Peek() > 0)
                 {
-                    credentials.Email = _isEncrypted ? _crypto.Decrypt(reader.ReadLine()) : reader.ReadLine();
-                    credentials.Password = _isEncrypted ? _crypto.Decrypt(reader.ReadLine()) : reader.ReadLine();
+                    credentials.Email = isEncrypted ? _crypto.Decrypt(reader.ReadLine()) : reader.ReadLine();
+                    credentials.Password = isEncrypted ? _crypto.Decrypt(reader.ReadLine()) : reader.ReadLine();
                     credentials.RememberMe = !string.IsNullOrEmpty(credentials.Password) && Convert.ToBoolean(reader.ReadLine());
                 }
                 reader.Close();
             }
             
-            if ((credentials.Email == null || credentials.Password == null) || (credentials.Email.Length > 20 || credentials.Password.Length > 20))
+            if (credentials.Email == null || credentials.Password == null || credentials.Password.Length > 20)
                 return new Credentials();
             return credentials;
         }
