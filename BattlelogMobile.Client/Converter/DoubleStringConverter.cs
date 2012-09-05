d1c62b9d4d49eca0a9d11b212539474b4f811b3a
Mikko Viitala <mikko.viitala@nbl.fi>
2012-09-05 22:19:43 +0300

    Changed converter

--- BattlelogMobile.Client/Converter/DoubleStringConverter.cs
+++ BattlelogMobile.Client/Converter/DoubleStringConverter.cs
@@ -16,18 +16,8 @@ namespace BattlelogMobile.Client.Converter
         /// </summary>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            bool wasNumeric = false;
-            string ratioFormat = RatioFormat;
+            string formatted = System.Convert.ToDouble(value, CultureInfo.InvariantCulture).ToString(RatioFormat, CultureInfo.InvariantCulture);
             if (parameter != null)
-            {
-                double doubleValue;
-                wasNumeric = double.TryParse(parameter.ToString(), out doubleValue);
-                if (wasNumeric)
-                    ratioFormat = parameter.ToString();
-            }
-
-            string formatted = System.Convert.ToDouble(value, CultureInfo.InvariantCulture).ToString(ratioFormat, CultureInfo.InvariantCulture);
-            if (parameter != null && !wasNumeric)
                 formatted += parameter.ToString();
             
             return formatted;
