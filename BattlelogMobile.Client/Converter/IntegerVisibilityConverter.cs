0f208360beb49777ff58b9cbca853ce8796cea77
Mikko Viitala <mikko.viitala@nbl.fi>
2012-12-03 13:03:31 +0200

    Gadget enhancements

--- BattlelogMobile.Client/Converter/IntegerVisibilityConverter.cs
+++ BattlelogMobile.Client/Converter/IntegerVisibilityConverter.cs
@@ -1,19 +1,27 @@
 ﻿using System;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 
 namespace BattlelogMobile.Client.Converter
 {
-    public class NegativeIntegerStringConverter : IValueConverter
+    public class IntegerVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is int)
             {
                 int intValue = int.Parse(value.ToString());
-                return intValue < 0 ? "-" : value.ToString();
+                return intValue >= 0 ? Visibility.Visible : Visibility.Collapsed;
             }
             throw new ArgumentException("Value must be type of int", "value");
+
+            //if (value is int)
+            //{
+            //    int intValue = int.Parse(value.ToString());
+            //    return intValue < 0 ? "" : value.ToString();
+            //}
+            //throw new ArgumentException("Value must be type of int", "value");
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
