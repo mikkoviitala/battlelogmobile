8908eca88a246f01f52508a1580fedf7d2175766
Mikko Viitala <mikko.viitala@nbl.fi>
2012-12-03 11:42:35 +0200

    New converter for gadget needs

---
+++ BattlelogMobile.Client/Converter/NegativeIntegerStringConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace BattlelogMobile.Client.Converter
+{
+    public class NegativeIntegerStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int)
+            {
+                int intValue = int.Parse(value.ToString());
+                return intValue < 0 ? "-" : value.ToString();
+            }
+            throw new ArgumentException("Value must be type of int", "value");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
