114ad370d93a9bf4b2ea19414874865a6a7d4f79
Mikko Viitala <mikko.viitala@nbl.fi>
2014-03-11 10:57:31 +0200

    Added, changed and removed common URIs

--- BattlelogMobile.Core/Common.cs
+++ BattlelogMobile.Core/Common.cs
@@ -17,7 +17,6 @@
         public const string HttpUserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0.2) Gecko/20100101 Firefox/6.0.2";
         public const string HttpContentType = "application/x-www-form-urlencoded";
 
-        //public const string SoldierFile = "soldier.bin";
         public const string CredentialsFile = "credentials.txt";
         public const string OverviewFile = "overview.txt";
         public const string VehiclesFile = "vehicles.txt";
@@ -29,25 +28,21 @@
         public const string VehiclesPageUrl = "http://battlelog.battlefield.com/bf3/vehiclesPopulateStats/{0}/{1}/";
         public const string GadgetsPageUrl = "http://battlelog.battlefield.com/bf3/weaponsPopulateStats/{0}/{1}/";
         public const string UnlocksPageUrl = "http://battlelog.battlefield.com/bf3/upcomingUnlocksPopulateStats/{0}/{1}/";
-        //public const string BattleFeedPageUrl = "http://battlelog.battlefield.com/bf3/";
 
         public const string GravatarImageUrl = "http://www.gravatar.com/avatar/{0}?s=52&d=http://battlelog-cdn.battlefield.com/public/base/shared/default-avatar-52.png?v=7909";
-        //public const string GravatarImageUrl = "http://www.gravatar.com/avatar/{0}";
-        //public const string DefaultGravatarImage = "default-avatar-52.png";
-        //public const string DefaultGravatarImageUrl = "http://battlelog-cdn.battlefield.com/public/base/shared/{0}?v=7909";
 
         public const int MaxRank = 45;
         public const string RankImagePrefix = "r";
         public const string RankServiceStarImagePrefix = "ss";
-        public const string RankImageUrl = "http://battlelog-cdn.battlefield.com/public/profile/bf3/stats/ranks/medium/{0}";
+        //public const string RankImageUrl = "http://battlelog-cdn.battlefield.com/public/profile/bf3/stats/ranks/medium/{0}";
         public const string KitImageUrl = "http://battlelog-cdn.battlefield.com/public/profile/bf3/kit-icon-{0}?v=3173239";
-        public const string WeaponAndAccessoryImageUrl = "http://battlelog-cdn.battlefield.com/public/profile/bf3/stats/items_90x54/{0}?v=3173239";
-        public const string RibbonAwardImageUrl = "http://battlelog-cdn.battlefield.com/public/profile/bf3/stats/ribbons/s/{0}?v=3173239";
-        public const string MedalAwardImageUrl = "http://battlelog-cdn.battlefield.com/public/profile/bf3/stats/medals/s/{0}?v=3173239";
-        public const string VehicleAndGadgetImageUrl = "http://battlelog-cdn.battlefield.com/public/profile/bf3/stats/items_147x88/{0}?v=3173239";
+        public const string WeaponAndAccessoryImageUrl = "http://eaassets-a.akamaihd.net/bl-cdn/cdnprefix/a162802f7633bb6127b7d9bda2b88f65c/public/profile/bf3/stats/items_147x88/{0}"; //"http://battlelog-cdn.battlefield.com/public/profile/bf3/stats/items_90x54/{0}?v=3173239";
+        public const string RibbonAwardImageUrl = "http://eaassets-a.akamaihd.net/bl-cdn/cdnprefix/a162802f7633bb6127b7d9bda2b88f65c/public/profile/bf3/stats/ribbons/s/{0}"; //"http://battlelog-cdn.battlefield.com/public/profile/bf3/stats/ribbons/s/{0}?v=3173239";
+        public const string MedalAwardImageUrl = "http://eaassets-a.akamaihd.net/bl-cdn/cdnprefix/a162802f7633bb6127b7d9bda2b88f65c/public/profile/bf3/stats/medals/s/{0}"; //"http://battlelog-cdn.battlefield.com/public/profile/bf3/stats/medals/s/{0}?v=3173239";
+        public const string VehicleAndGadgetImageUrl = "http://eaassets-a.akamaihd.net/bl-cdn/cdnprefix/a162802f7633bb6127b7d9bda2b88f65c/public/profile/bf3/stats/items_147x88/{0}"; //"http://battlelog-cdn.battlefield.com/public/profile/bf3/stats/items_147x88/{0}?v=3173239";
 
-        public const string ServiceStarImage = "servicestar-31x31.png";
-        public const string ServiceStarImageUrl = "http://battlelog.battlefield.com/public/profile/bf3/stats/servicestars/{0}";
+        //public const string ServiceStarImage = "servicestar-31x31.png";
+        //public const string ServiceStarImageUrl = "http://battlelog.battlefield.com/public/profile/bf3/stats/servicestars/{0}";
 
         public const string KillsToken = "kills";
         public const string AwardGroup = "AwardGroup_Ribbons";
