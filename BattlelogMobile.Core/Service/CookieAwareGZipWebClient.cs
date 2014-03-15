54dae9f455fe4b019973fe115b0a9790c2d82d86
Mikko Viitala <mikko.viitala@nbl.fi>
2014-03-15 22:49:18 +0200

    Added cookie support to SharpGIS GZipWebClient

---
+++ BattlelogMobile.Core/Service/CookieAwareGZipWebClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using SharpGIS;
+
+namespace BattlelogMobile.Core.Service
+{
+    public class CookieAwareGZipWebClient : GZipWebClient
+    {
+        public CookieAwareGZipWebClient()
+        {}
+        
+        public CookieAwareGZipWebClient(CookieContainer cookieCookieJar)
+        {
+            CookieJar = cookieCookieJar;
+        }
+
+        public CookieContainer CookieJar { get; set; }
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            WebRequest webRequest = base.GetWebRequest(address);
+            var request = webRequest as HttpWebRequest;
+            if (request != null)
+            {
+                request.CookieContainer = CookieJar;
+            }
+            return webRequest;
+        }
+    }
+}
