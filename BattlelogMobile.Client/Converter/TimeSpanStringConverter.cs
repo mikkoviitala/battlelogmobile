0c6bb5a787de26cf57347de16347748ff6e12d8d
Mikko Viitala <mikko.viitala@nbl.fi>
2014-03-16 01:53:41 +0200

    Added optional formatting

--- BattlelogMobile.Client/Converter/TimeSpanStringConverter.cs
+++ BattlelogMobile.Client/Converter/TimeSpanStringConverter.cs
@@ -9,22 +9,39 @@ namespace BattlelogMobile.Client.Converter
     /// </summary>
     public class TimeSpanStringConverter : IValueConverter
     {
-        private const string Format = "00";
-        private const string Hours = "h ";
-        private const string Minutes = "m";
-
-        /// <summary>
-        /// Return hours and minutes, format is "00h 00m";
-        /// </summary>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var span = (TimeSpan)value;
-            return ((span.Days * 24) + span.Hours).ToString(Format) + Hours + span.Minutes.ToString(Format) + Minutes;
+
+            if (parameter == null || string.CompareOrdinal(parameter.ToString(), "full") != 0)
+                return FormatAsHoursAndMinutes(span);
+            return FormatAsCompleteUnits(span);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
+        }
+
+        private static string FormatAsHoursAndMinutes(TimeSpan span)
+        {
+            return string.Format("{0}h {1}m", ((span.Days * 24) + span.Hours).ToString("00"), span.Minutes.ToString("00"));
+        }
+
+        private static string FormatAsCompleteUnits(TimeSpan span)
+        {
+            long days = (long) Math.Floor(span.TotalDays);
+            long hours = (long) Math.Floor(span.TotalHours);
+            long minutes = (long) Math.Floor(span.TotalMinutes);
+
+            if (days > 0)
+                return string.Format(days == 1 ? "{0} day" : "{0} days", days);
+            if (hours > 0)
+                return string.Format(hours == 1 ? "{0} hour" : "{0} hours", hours);
+            if (minutes > 0)
+                return string.Format(minutes == 1 ? "{0} minute" : "{0} minutes", minutes);
+
+            return "Less than a minute";
         }
     }
 }
