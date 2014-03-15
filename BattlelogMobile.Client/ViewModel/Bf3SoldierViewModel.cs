35986ee47b55e7c47d8b47d3c6dbbd11cfb85d20
Mikko Viitala <mikko.viitala@nbl.fi>
2014-03-16 01:55:03 +0200

    Replaced header and added last update time

--- BattlelogMobile.Client/ViewModel/Bf3SoldierViewModel.cs
+++ BattlelogMobile.Client/ViewModel/Bf3SoldierViewModel.cs
@@ -1,10 +1,29 @@
-﻿using BattlelogMobile.Core.Model;
+﻿using System;
+using System.Windows.Threading;
+using BattlelogMobile.Core.Model;
 
 namespace BattlelogMobile.Client.ViewModel
 {
     public class Bf3SoldierViewModel : BaseViewModel
     {
         private Battlefield3Data _battlefield3Data;
+        private readonly DispatcherTimer _dispatchTimer;
+
+        public Bf3SoldierViewModel()
+        {
+            _dispatchTimer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1d) };
+            _dispatchTimer.Tick += (sender, args) =>
+                {
+                    if (Data == null)
+                        return;
+
+                    Updated = DateTime.Now - Data.Updated;
+                    RaisePropertyChanged("Updated");
+                };
+            _dispatchTimer.Start();
+        }
+
+        public TimeSpan Updated { get; private set; }
 
         public Battlefield3Data Data
         {
