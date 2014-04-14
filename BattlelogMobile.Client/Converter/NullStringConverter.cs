e688cf11409c59797f2e66e116b3406064d2d2f8
Mikko Viitala <mikko.viitala@nbl.fi>
2014-04-14 22:22:09 +0300

    New converters

---
+++ BattlelogMobile.Client/Converter/NullStringConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace BattlelogMobile.Client.Converter
+{
+    public class NullStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter != null && value == null)
+                return parameter;
+            if (value == null)
+                return "null";
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BattlelogMobile.Client/Converter/PathImageSourceConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace BattlelogMobile.Client.Converter
+{
+    public class PathImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string path = @"/Assets/Images/Battlefield4/" + value;
+            Uri uri = new Uri(path, UriKind.Relative);
+            BitmapImage imgSource = new BitmapImage(uri);
+            return imgSource as ImageSource;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Stream ReadFile(string filePath)
+        {
+            var resrouceStream = Application.GetResourceStream(new Uri(filePath, UriKind.Relative));
+            if (resrouceStream != null)
+            {
+                using (var stream = resrouceStream.Stream)
+                {
+                    if (stream.CanRead)
+                    {
+                        var myStreamReader = new StreamReader(stream);
+                        var b = myStreamReader.ReadToEnd();
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
